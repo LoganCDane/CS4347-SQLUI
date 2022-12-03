90adb84cf2a87f9ff72130b8832efad63891962c
LoganCDane <logan.dane58@k12.leanderisd.org>
2022-12-03 17:40:43 -0600

    Added two options and fixed click errors that would occur when your mouse button was still held when reaching a screen.

--- src/Runner.java
+++ src/Runner.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 
 public class Runner {
     static MySQLAccess sql = new MySQLAccess();
+
     public static void main(String[] args) throws Exception {
         StdDraw.enableDoubleBuffering();
         StdDraw.setTitle("LolBase Connection");
@@ -18,25 +19,39 @@ public class Runner {
 
         ArrayList<Button> buttons = new ArrayList<Button>();
         int center = 50;
-        int hh = 10;
-        buttons.add(new Button(center, center+(hh*3), 25, 10, Color.BLUE, "SELECT"));
-        buttons.add(new Button(center, center, 25, 10, Color.BLUE, "UPDATE"));
-        buttons.add(new Button(center, center-(hh*3), 25, 10, Color.BLUE, "Prepared Statement"));
-        
+        int hh = 7;
+        buttons.add(new Button(center, center+(hh*6), 25, hh, Color.BLUE, "SELECT"));
+        buttons.add(new Button(center, center+(hh*3), 25, hh, Color.blue, "INSERT" ));
+        buttons.add(new Button(center, center, 25, hh, Color.BLUE, "UPDATE"));
+        buttons.add(new Button(center, center-(hh*3), 25, hh, Color.blue, "DELETE" ));
+        buttons.add(new Button(center, center-(hh*6), 25, hh, Color.BLUE, "Prepared Statement"));
+        boolean prevClick = false;
         while (true) {
 
             for (int i = 0; i < buttons.size(); i++) {
+                if(!StdDraw.isMousePressed()) prevClick = false;
                 Button button = buttons.get(i);
-                if (StdDraw.isMousePressed() && button.inBounds(StdDraw.mouseX(), StdDraw.mouseY())) {
+                if (!prevClick && StdDraw.isMousePressed() && button.inBounds(StdDraw.mouseX(), StdDraw.mouseY())) {
                     //System.out.println(Arrays.deepToString(sql.readDatabase2()));
                     if(i == 0){
                         CommandWindow("SELECT");
+                        prevClick = true;
                     }
-                    else if (i == 1){
+                    else if(i == 1){
+                        CommandWindow("INSERT into");
+                        prevClick = true;
+                    }
+                    else if (i == 2){
                         CommandWindow("UPDATE");
+                        prevClick = true;
                     }
-                    else if(i == 2){
-
+                    else if (i == 3){
+                        CommandWindow("DELETE from");
+                        prevClick = true;
+                    }
+                    else if(i == 4){
+                        //TODO: Add Prepared statements
+                        prevClick = true;
                     }
                 }
                 button.draw();
@@ -49,10 +64,12 @@ public class Runner {
     }
     
     private static void CommandWindow(String command){
+        Boolean prevClick = true;
         StringBuilder input = new StringBuilder();
         Button confirm = new Button(50, 30, 10, 10, Color.BLUE, "CONFIRM");
         Button back = new Button(30, 30, 5, 5, Color.red, "<-");
         while (true) {
+            if(!StdDraw.isMousePressed()) prevClick = false;
             while (StdDraw.hasNextKeyTyped()) {
                 char key = StdDraw.nextKeyTyped();
                 System.out.print(key);
@@ -63,14 +80,14 @@ public class Runner {
                     input.append(key);
                 }
             }
-            if(StdDraw.isMousePressed() && confirm.inBounds()){
+            if(!prevClick && StdDraw.isMousePressed() && confirm.inBounds()){
                 try {
                     System.out.println(Arrays.deepToString(sql.readDatabase(command + " " + input.toString())));
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
             }
-            else if(StdDraw.isMousePressed() && back.inBounds()){
+            else if(!prevClick && StdDraw.isMousePressed() && back.inBounds()){
                 return;
             }
 
@@ -88,7 +105,6 @@ public class Runner {
             StdDraw.clear();
         }
     }
-
 
 
 }
